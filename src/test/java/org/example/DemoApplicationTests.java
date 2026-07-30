fdad4d4287338bd31d080783deb4fba8914a25b4
afroza1717 <liz.hossain@gmail.com>
2026-07-29 22:40:28 -0400

    Added @WebMVC test cases

--- src/test/java/org/example/DemoApplicationTests.java
+++ src/test/java/org/example/DemoApplicationTests.java
@@ -27,10 +27,12 @@ class DemoApplicationTests {
         }
 
         @Container
-        static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
+        @SuppressWarnings("resource")
+        static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
                         .withDatabaseName("task_database")
                         .withUsername("myuser")
-                        .withPassword("secret");
+                        .withPassword("secret")
+                        .withReuse(true);
 
         @DynamicPropertySource
         static void configureProperties(DynamicPropertyRegistry registry) {
